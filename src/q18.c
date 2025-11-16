376c34c9d3b60b83f076d0cd528a018c20ce42a2
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-16 20:18:26 +0545

    Update q18.c

--- src/q18.c
+++ src/q18.c
@@ -1,1 +1,26 @@
-// Write a function named findPrimeNumbers that takes an integer n as input and prints all prime numbers from 1 to n.
+// Write a function named findPrimeNumbers that takes an integer n as input and prints all prime numbers from 1 to n.#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+void findPrimeNumbers(int n) {
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n = 20;
+    findPrimeNumbers(n);
+    return 0;
+}
