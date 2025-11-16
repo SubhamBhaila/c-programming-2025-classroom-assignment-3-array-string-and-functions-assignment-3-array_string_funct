e9453fb4a55369026b5b8422ace54f6274ce8e5d
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-16 20:15:07 +0545

    Update q15.c

--- src/q15.c
+++ src/q15.c
@@ -1,1 +1,25 @@
-// Write a function named countOccurrences that takes a string and a character as input and returns the number of times the character appears in the string.
+#include <stdio.h>
+
+int countOccurrences(const char *str, char ch) {
+    int count = 0;
+    int i = 0;
+    
+    while (str[i] != '\0') {
+        if (str[i] == ch) {
+            count++;
+        }
+        i++;
+    }
+    
+    return count;
+}
+
+int main() {
+    char text[] = "hello world";
+    char character = 'l';
+    
+    int result = countOccurrences(text, character);
+    printf("The character '%c' appears %d times.\n", character, result);
+    
+    return 0;
+}
