d171a63581beea84a603d5b6bc2cf869abc49256
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-16 20:12:17 +0545

    Update q12.c

--- src/q12.c
+++ src/q12.c
@@ -1,1 +1,21 @@
 //  Write a function named isPalindrome that takes a string as input and returns 1 if it is a palindrome (reads the same forwards and backwards), and 0 otherwise.
+#include <stdio.h>
+
+int isPalindrome(char str[]) {
+    int i = 0, j = 0;
+
+    while (str[j] != '\0') {
+        j++;
+    }
+    j--;
+
+    while (i < j) {
+        if (str[i] != str[j]) {
+            return 0;
+        }
+        i++;
+        j--;
+    }
+
+    return 1;
+}
