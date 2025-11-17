4c7df9ab085a9f4ffba68f9509f630cd435804a4
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-17 18:07:18 +0545

    Update q23.c

--- src/q23.c
+++ src/q23.c
@@ -1,1 +1,18 @@
-// Write a recursive function named calculateGCD that takes two integers a and b as input and returns their greatest common divisor (GCD).
+// Write a recursive function named calculateGCD that takes two integers a and b as input and returns their greatest common divisor (GCD).#include <stdio.h>
+
+int calculateGCD(int a, int b) {
+    if (b == 0)
+        return a;
+    return calculateGCD(b, a % b);
+}
+
+int main() {
+    int x, y;
+    printf("Enter two integers: ");
+    scanf("%d %d", &x, &y);
+
+    int gcd = calculateGCD(x, y);
+    printf("GCD = %d\n", gcd);
+
+    return 0;
+}
