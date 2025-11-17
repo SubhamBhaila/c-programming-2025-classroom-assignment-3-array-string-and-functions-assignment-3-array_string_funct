fcdd26df6074ca3fbfacc40600cf32f0872e087a
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-17 18:06:03 +0545

    Update q20.c

--- src/q20.c
+++ src/q20.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int calculateGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int x, y;
+    printf("Enter two integers: ");
+    scanf("%d %d", &x, &y);
+
+    int gcd = calculateGCD(x, y);
+    printf("GCD = %d\n", gcd);
+
+    return 0;
+}
 // Write a function named calculateGCD that takes two integers as input and returns their greatest common divisor (GCD).
