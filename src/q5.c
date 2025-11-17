bc33d9cb8fad77d28e8b4d1f16988e2e3cbd1b89
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-17 18:12:09 +0545

    Update q5.c

--- src/q5.c
+++ src/q5.c
@@ -1,1 +1,42 @@
-// Write a C program to merge two sorted arrays into a single sorted array.
+// Write a C program to merge two sorted arrays into a single sorted array.#include <stdio.h>
+
+int main() {
+    int n1, n2, i, j, k;
+
+    printf("Enter number of elements in first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+    printf("Enter %d elements of first array (sorted):\n", n1);
+    for (i = 0; i < n1; i++)
+        scanf("%d", &arr1[i]);
+
+    printf("Enter number of elements in second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+    printf("Enter %d elements of second array (sorted):\n", n2);
+    for (i = 0; i < n2; i++)
+        scanf("%d", &arr2[i]);
+
+    int merged[n1 + n2];
+    i = j = k = 0;
+
+    while (i < n1 && j < n2) {
+        if (arr1[i] <= arr2[j])
+            merged[k++] = arr1[i++];
+        else
+            merged[k++] = arr2[j++];
+    }
+
+    while (i < n1)
+        merged[k++] = arr1[i++];
+
+    while (j < n2)
+        merged[k++] = arr2[j++];
+
+    printf("Merged sorted array:\n");
+    for (i = 0; i < n1 + n2; i++)
+        printf("%d ", merged[i]);
+    printf("\n");
+
+    return 0;
+}
