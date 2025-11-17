bd4470405c6c44b8750f677605a0223d6fe7071c
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-17 18:05:08 +0545

    Update q19.c

--- src/q19.c
+++ src/q19.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 
-// Function to calculate and print factorial series up to n
 void calculateFactorialSeries(int n) {
     int i;
-    long long fact = 1;  // use long long to handle bigger values
-
-    printf("Factorial series up to %d:\n", n);
+    long long fact = 1;
     for (i = 1; i <= n; i++) {
-        fact = fact * i;      // update factorial
-        printf("%lld ", fact); // print current factorial
+        fact = fact * i;
+        printf("%lld ", fact);
     }
     printf("\n");
 }
 
-// Example main function to test
 int main() {
     int n;
-
     printf("Enter a number: ");
     scanf("%d", &n);
-
     calculateFactorialSeries(n);
-
     return 0;
 }
