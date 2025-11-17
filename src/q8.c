350aaed439b00aa412efd24a718d61309b66918b
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-17 18:13:15 +0545

    Update q8.c

--- src/q8.c
+++ src/q8.c
@@ -1,1 +1,29 @@
-// Write a C program to check if a given string is a palindrome.
+// Write a C program to check if a given string is a palindrome.#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, length, isPalindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
