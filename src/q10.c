5ce9636d169921aed2f6bd99249efeee6db0f6bc
SubhamBhaila <subhambhaila10@gmail.com>
2025-11-22 12:45:48 +0545

    Update q10.c

--- src/q10.c
+++ src/q10.c
@@ -6,17 +6,32 @@ int main() {
     int i = 0, j = 0;
 
     printf("Enter first string: ");
-    scanf("%s", str1);
+    fgets(str1, sizeof(str1), stdin);
 
     printf("Enter second string: ");
-    scanf("%s", str2);
+    fgets(str2, sizeof(str2), stdin);
+
+    while (str1[i] != '\0') {
+        if (str1[i] == '\n') {
+            str1[i] = '\0';
+            break;
+        }
+        i++;
+    }
 
     while (str1[i] != '\0') {
         i++;
     }
 
-    while (str2[j] != '\0') {
+    while (str2[j] != '\0' && str2[j] != '\n') {
         str1[i] = str2[j];
         i++;
         j++;
     }
+
+    str1[i] = '\0';
+
+    printf("Concatenated string: %s\n", str1);
+
+    return 0;
+}
